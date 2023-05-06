f8f8ea770aaacef105397321a2380665b45c3d17
Mihir Aswal <montyaswal0@gmail.com>
2023-05-06 15:53:55 +0530

    install dependenices

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import userRoutes from'./routes/users.js';
 import postRoutes from './routes/post.js';
 import { verifyToken } from './middleware/auth.js';
 import { createPost } from './controller/posts.js';
+import User from './models/User.js'
+import Post from './models/Post.js';
+import { users,posts } from './data/index.js';
 //middleware configuration
 const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
