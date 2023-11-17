f944fdcab5f2ae6ecb7b7d8563cbb0c8ae723228
Mihir Aswal <montyaswal0@gmail.com>
2023-11-17 07:09:20 +0530

    deply

--- server/index.js
+++ server/index.js
@@ -24,19 +24,13 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet({
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      connectSrc: ["'self'", 'https://socio-app-0k5t.onrender.com']
-    }
-  }
-}));
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
@@ -63,20 +57,11 @@ app.use("/posts", postRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  );
-} else {
   app.get("/", (req, res) => {
     res.send("API is running..");
+ 
   });
-}
-
-
 
 
 
