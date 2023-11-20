95bff52532d83327631d25583263297a88619a8f
Mihir Aswal <montyaswal0@gmail.com>
2023-11-21 00:56:25 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -62,20 +62,10 @@ app.use("/posts", postRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname1, "/client/build")));
-
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-);
-} else {
 app.get("/", (req, res) => {
+  res.setHeader("Access-control-Allow-Origin", "*");
   res.send("API is running..");
 });
-}
-
 
 
 
